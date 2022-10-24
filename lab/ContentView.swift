af54d23d7a21e837d307791828c1a8aebd858b57
Alaa <Allo.busy@icloud.com>
2022-10-24 10:03:04 +0300

    Initial Commit

---
+++ lab/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  lab
+//
+//  Created by Alaa Alabdullah on 24/10/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!!!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(/*@START_MENU_TOKEN@*/.light/*@END_MENU_TOKEN@*/)
+    }
+}
---
+++ lab/labApp.swift
@@ -0,0 +1,17 @@
+//
+//  labApp.swift
+//  lab
+//
+//  Created by Alaa Alabdullah on 24/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct labApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
